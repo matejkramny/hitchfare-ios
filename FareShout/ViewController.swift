17771037ee66ae670c14f3ec671fa9670bb4caa0
Matej Kramny <matejkramny@gmail.com>
2014-10-27 17:15:05 +0000

    Initial Commit

---
+++ FareShout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FareShout
+//
+//  Created by Matej Kramny on 27/10/2014.
+//  Copyright (c) 2014 Matej Kramny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
